e38e75567af8f95c3ffae5f3690f8bd087b8255b
Brigimar <160976236+brigimar@users.noreply.github.com>
2024-02-23 19:03:04 -0300

    Update env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,4 +1,5 @@
-// eslint-disable-next-line @typescript-eslint/triple-slash-reference
-/// <reference path="../.astro/types.d.ts" />
-/// <reference types="astro/client" />
-/// <reference types="vite/client" />
+interface ImportMetaEnv {
+  readonly CONTENTFUL_SPACE_ID: string;
+  readonly CONTENTFUL_DELIVERY_TOKEN: string;
+  readonly CONTENTFUL_PREVIEW_TOKEN: string;
+}
